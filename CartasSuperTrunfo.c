18eaf0e6bf61e43c07620ddd7032c2ed48d28e14
CARLOS RODOLFO OLIVEIRA SOUSA <rdoliveirasousa@hotmail.com>
2025-08-28 01:32:54 +0000

    Inclui a estrutura de condição comparando itens das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -89,61 +89,25 @@ int main() {
 
     printf("\n");
 
-    //No código abaixo será impresso todos os dados que foram armazenados em cada variável da primeira carta.
+    printf("Densidade Demografica Carta 1: %.2f\n", DensidadePop1);
+    printf("Densidade Demografica Carta 2: %.2f\n", DensidadePop2);
+    printf("PIB Per Capita Carta 1: %.2f\n", PIBperCapita1);
+    printf("PIB Per Capita Carta 2: %.2f\n", PIBperCapita2);
     
-    printf("Carta 1: \n");
-    printf("Estado: %s\n", Estado1);
-    printf("Código: %s\n", Codigo_Carta1);
-    printf("Nomde da Cidade: %s\n", Nome_Cidade1);
-    printf("População: %d\n", Populacao1);
-    printf("Área: %.2f km²\n", Area1);
-    printf("PIB: %.2f bilhões de reais\n", PIB1);
-    printf("Número de pontos Turísticos: %d\n", Ponto_Turistico1);
-    printf("Densidade Demográfica: %.2f hab/km²\n", DensidadePop1);
-    printf("PIB per Capita: %.2f reais\n", PIBperCapita1);
-
-    printf("\n"); //utilizado para saltar uma linha
-
-    //No código abaixo será impresso todos os dados que foram armazenados em cada variável da segunda carta.    
-
-    printf("Carta 2: \n");
-    printf("Estado: %s\n", Estado2);
-    printf("Código: %s\n", Codigo_Carta2);
-    printf("Nomde da Cidade: %s\n", Nome_Cidade2);
-    printf("População: %d\n", Populacao2);
-    printf("Área: %.2f km²\n", Area2);
-    printf("PIB: %.2f bilhões de reais\n", PIB2);
-    printf("Número de pontos Turísticos: %d\n", Ponto_Turistico2);
-    printf("Densidade Demográfica: %.2f hab/km²\n", DensidadePop2);
-    printf("PIB per Capita: %.2f reais\n", PIBperCapita2);
 
     printf("\n");
 
-    //Soma dos Super Poderes das cartas 1 e 2
-    float SuperPoder1 = (float) Populacao1 + Area1 + PIB1 + PIBperCapita1 + (1/DensidadePop1) + Ponto_Turistico1;
-    float SuperPoder2 = (float) Populacao2 + Area2 + PIB2 + PIBperCapita2 + (1/DensidadePop2) + Ponto_Turistico2;
+    //Comparação entre itens das cartas utilizando a estrutura de condição.
 
-    //Declaração das variáveis resultdos das comparações
-    int resultadoPop, resultadoArea, resultadoPIB, resultadoDensidade, resultadoPerCapita, resultadoSuperPoder;
+    printf("Comparação entre cartas (Atributo: População):\n");
+    printf("Carta 1 - %s (%s): %d\n", Estado1, Codigo_Carta1, Populacao1);
+    printf("Carta 2 - %s (%s): %d\n", Estado2, Codigo_Carta2, Populacao2);
 
-    //Comparação dos dados das duas cartas
-    resultadoPop = Populacao1 > Populacao2;
-    resultadoArea = Area1 > Area2;
-    resultadoPIB = PIB1 > PIB2;
-    resultadoDensidade = DensidadePop1 > DensidadePop2;
-    resultadoPerCapita = PIBperCapita1 >  PIBperCapita2;
-    resultadoSuperPoder = SuperPoder1 > SuperPoder2;
-
-    //Saída de dados das comparações das cartas. Sendo (1) para carta 1 e (0) para carta 2
-    printf("#Comparação das cartas#\n");
-    printf("Carta 1 vence com resultado (1)\n");
-    printf("Carta 2 vence com resultado (0)\n");
-    printf("População: (%d)\n", resultadoPop);
-    printf("Área: (%d)\n", resultadoArea);
-    printf("PIB:: (%d)\n", resultadoPIB);
-    printf("Densidade Demográfica: (%d)\n", resultadoDensidade);
-    printf("PIB Per Capita: (%d)\n", resultadoPerCapita);
-    printf("Super Poder: (%d)\n", resultadoSuperPoder);
+    if (Populacao1 > Populacao2) {
+        printf("Resultado: Carta 1 (%s) venceu!\n", Estado1);
+    } else {
+        printf("Resultado: Carta 2 (%s) venceu!\n", Estado2);
+    }
 
     printf("\n");
 
