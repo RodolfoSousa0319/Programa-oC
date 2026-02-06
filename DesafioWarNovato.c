313e805823ea3a0520317d6e4b4a4f9c8d4276b5
CARLOS RODOLFO OLIVEIRA SOUSA <rdoliveirasousa@hotmail.com>
2026-02-06 21:56:50 +0000

    printf cabeçalho

---
+++ DesafioWarNovato.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+struct territorio {
+        char nome [30];
+        char cor [10];
+        int tropa;
+
+    };
+
+int main() {
+    
+    printf ("========================== \n");
+    printf ("  Cadastro do Território \n");
+    printf ("========================== \n");
+
+
+   return 0; 
+}
