aec7d6d08ed9a37b4f8a6678f19ea6843407e459
CARLOS RODOLFO OLIVEIRA SOUSA <rdoliveirasousa@hotmail.com>
2026-02-07 04:03:56 +0000

    dados dos territorios

--- DesafioWarNovato.c
+++ DesafioWarNovato.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
-struct territorio {
+typedef struct {
         char nome [30];
         char cor [10];
         int tropa;
 
-    };
+    }territorio;
 
 int main() {
     
+    territorio territorio1;
+
     printf ("========================== \n");
     printf ("  Cadastro do Território \n");
     printf ("========================== \n");
-    printf ("--Cadastre 5 territórios-- \n");
+    printf ("Vamos cadastrar os 5 territórios iniciais do nosso mundo.\n");
+
+    int i;
+    for ( int i = 1; i <= 6; i++) {
+
+        printf ("---Cadastrando Território%d" , i , "---");
+        printf ("\n");
+        printf ("Nome do território: ");
+        scanf ("%s",&territorio1.nome);        
+        printf ("Cor do Exército (ex: azul, verde...): ");
+        scanf ("%s",&territorio1.cor);
+        printf ("Número de Tropas: ");
+        scanf ("%d",&territorio1.tropa);
+
+    }
+
+    printf ("Nome do Território", territorio1.nome);
+    printf ("Cor do Exército:", territorio1.cor);
+    printf ("Número de Tropas", territorio1.tropa);
+
+    
     
 
-
-   return 0; 
+   return 0;
+   
 }
