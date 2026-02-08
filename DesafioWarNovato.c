ec36e04b72a320730206f67707ad3f4ff9ce2be0
CARLOS RODOLFO OLIVEIRA SOUSA <rdoliveirasousa@hotmail.com>
2026-02-08 04:04:33 +0000

    Criei o vetor/leitura de dados/encapsulamento

--- DesafioWarNovato.c
+++ DesafioWarNovato.c
@@ -1,42 +1,58 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct {
+
+//inserção de struct territorio
+struct territorio {
         char nome [30];
         char cor [10];
         int tropa;
 
-    }territorio;
-
-int main() {
+    }territorio [5];  
     
-    territorio territorio1;
+       
 
+int main() {       
+    
+    int i;  
+    
     printf ("========================== \n");
     printf ("  Cadastro do Território \n");
     printf ("========================== \n");
-    printf ("Vamos cadastrar os 5 territórios iniciais do nosso mundo.\n");
 
-    int i;
-    for ( int i = 1; i <= 6; i++) {
+    
+    printf ("\nVamos cadastrar os 5 territórios iniciais do nosso mundo.\n");   
+    
+    //coleta de dados 
+    for (i = 1; i < 6; i++) {        
+        
+        printf ("\n\n---- Cadastrando Território %d -----\n" , i);        
 
-        printf ("---Cadastrando Território%d" , i , "---");
-        printf ("\n");
         printf ("Nome do território: ");
-        scanf ("%s",&territorio1.nome);        
+        scanf ("%s", &territorio[i].nome);
+
         printf ("Cor do Exército (ex: azul, verde...): ");
-        scanf ("%s",&territorio1.cor);
-        printf ("Número de Tropas: ");
-        scanf ("%d",&territorio1.tropa);
+        scanf ("%s", &territorio[i].cor);
 
-    }
+        printf ("Número de Tropas: ");        
+        scanf ("%d", &territorio[i].tropa);
 
-    printf ("Nome do Território", territorio1.nome);
-    printf ("Cor do Exército:", territorio1.cor);
-    printf ("Número de Tropas", territorio1.tropa);
-
+    } //fim coleta de dados
     
-    
+
+    printf ("\n\n=============================================\n");
+    printf ("        MAPA DO MUNDO = ESTADO ATUAL         \n");
+    printf ("=============================================\n\n\n");
+
+    //percorrer o vetor territorio
+    for (i = 1; i < 6; i++ ) {
+
+    printf ("TERRITORIO %d:\n" , i );     
+    printf ("   - Nome: %s\n", territorio[i].nome);
+    printf ("   - Cor: %s\n", territorio[i].cor);
+    printf ("   - Tropas: %d\n\n", territorio[i].tropa);
+
+    }  
 
    return 0;
    
